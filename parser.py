5093f82a15a938e99e8e065b3f6e101bdf37c6fe
Любовь <189024783+DocKlaus@users.noreply.github.com>
2025-06-26 14:35:28 +0300

    Первичная неоптимизированная логика
    добавлены функции получения текста из тегов и объектом (ед., мн.). + логирование

--- parser.py
+++ parser.py
@@ -5,6 +5,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time
+import logging
+
+# Настройка логирования
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()],
+)
+
+logger = logging.getLogger(__name__)
+
 
 # Настройка Selenium
 options = Options()
@@ -14,6 +25,27 @@ options.add_argument(
 options.add_argument("--disable-blink-features=AutomationControlled")
 
 driver = webdriver.Chrome(options=options)
+
+
+def get_text(source, attribute, value):
+    result = source.find_element(By.CSS_SELECTOR, f"[{attribute}={value}]").text
+    return result
+
+
+def get_object(
+    source, attribute=None, value: str = "", get_list: bool = False
+) -> list | str:
+    if attribute is None:
+        arg = value
+    else:
+        arg = f"[{attribute}={value}]"
+
+    if not get_list:
+        result = source.find_element(By.CSS_SELECTOR, arg)
+        return result
+    else:
+        result = source.find_elements(By.CSS_SELECTOR, arg)
+        return result
 
 
 try:
@@ -26,40 +58,93 @@ try:
     )
 
     # Ищем только внутри этой секции
-    product_headers = pizza_section.find_elements(
-        By.CSS_SELECTOR, '[class="_2qj-DTg_kGPvRApDKha_-w"]'
+    product_headers = get_object(
+        source=pizza_section, attribute="class", value="_2qj-DTg_kGPvRApDKha_-w"
     )
 
     pizza_data = []
 
     for product in product_headers:
-        # Название
-        name = product.find_element(
-            By.CSS_SELECTOR, '[data-test-id="product_card_header"]'
-        ).text
+        dict = {}
 
-        # Описание
-        description = product.find_element(
-            By.CSS_SELECTOR, '[class="_2uYmw-6znBwRpeYTuDcvPN"]'
-        ).text
+        # Поиск кнопок выбора теста
+        dough_buttons = get_object(
+            source=product,
+            attribute=None,
+            value="._22tLg_N-T1_fSuHivc553F",
+            get_list=True,
+        )
+        logger.debug(f"dough_buttons: {len(dough_buttons)}")
 
-        # Извлекаем текст в отдельный список
-    pizza_names = [header.text for header in product_headers]
+        dough = None
 
-    # Выводим результат
-    print("Найдены следующие пиццы в разделе 'Пицца':")
-    for name in pizza_names:
-        print(f"- {name}")
+        # Проверяем кнопки
+        for button in dough_buttons:
+            class_attribute = button.get_attribute("class")
 
-    # Сохраняем в переменную
-    all_pizza_names = pizza_names
+            # Если кнопка уже активна (содержит gFWUICI_xCcypOmIgwq3L)
+            if "gFWUICI_xCcypOmIgwq3L" in class_attribute:
+                dough = button.text
+                logger.debug(f"dough: {dough}")
+
+            else:
+                # Нажимаем на неактивную кнопку (например, "Тонкое")
+                button.click()
+                time.sleep(1)  # Ждем обновления класса
+
+                # Проверяем, что кнопка стала активной
+                updated_class = button.get_attribute("class")
+                if "gFWUICI_xCcypOmIgwq3L" in updated_class:
+                    dough = button.text
+                    logger.debug(f"dough: {dough}")
+
+            # Здесь же надо найти диаметр и цену
+
+            # Находим блок с кнопками размеров
+            size_selector = get_object(
+                source=product, attribute="data-test-id", value="size_selector"
+            )
+            logger.debug(f"size_selector: {size_selector[:25]}")
+
+            # список кнопок в этом блоке
+            size_buttons = get_object(
+                source=size_selector, attribute="role", value="button", get_list=True
+            )
+            logger.debug(f"size_buttons: {len(size_buttons)}")
+
+            # Кликабельность кнопок
+            for size_button in size_buttons:
+                size_button_attribute = size_button.get_attribute("class")
+
+                if "_3ZxcheiXBqcNXPHFDFBcmo" in size_button_attribute:
+                    diameter = size_button.text
+                    logger.debug(f"size: {diameter}")
+
+                else:
+                    size_button.click()
+                    time.sleep(1)
+                    updated_size_b_attribute = size_button.get_attribute("class")
+                    if "_3ZxcheiXBqcNXPHFDFBcmo" in updated_size_b_attribute:
+                        diameter = size_button.text
+                        logger.debug(f"size: {diameter}")
+
+                dict["name"] = get_text(
+                    source=product,
+                    attribute="data-test-id",
+                    value="product_card_header",
+                )
+                dict["description"] = get_text(
+                    source=product, attribute="class", value="_2uYmw-6znBwRpeYTuDcvPN"
+                )
+                dict["dough"] = dough
+                dict["size"] = diameter
+                dict["price"] = get_text(
+                    source=product, attribute="data-test-id", value="amount_price"
+                )
+
 
 except Exception as e:
     print(f"Произошла ошибка: {e}")
 
 finally:
     driver.quit()
-
-# Теперь переменная all_pizza_names содержит только пиццы из нужной секции
-print("\nВсе названия из секции 'pizza':")
-print(all_pizza_names)
